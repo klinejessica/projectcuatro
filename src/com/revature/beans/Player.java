9e2c4d843647847e38954885697ed6afd61b989f
Josephajones <joseph.a.jones01@gmail.com>
2017-07-16 22:32:00 -0400

    Adding person impl

--- src/com/revature/beans/Player.java
+++ src/com/revature/beans/Player.java
@@ -3,5 +3,7 @@ package com.revature.beans;
 import com.revature.data.Person;
 
 public class Player implements Person{
-
+	public int getChoice(int numOfChoices){
+		return (int)(Math.random()*numOfChoices);
+	}
 }
--- src/com/revature/data/Person.java
+++ src/com/revature/data/Person.java
@@ -1,6 +1,5 @@
 package com.revature.data;
 
 public interface Person {
-	
-	
+	public int getChoice(int numOfChoices);
 }
